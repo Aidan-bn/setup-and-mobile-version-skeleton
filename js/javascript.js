9e5075c2fff1724b5bb0ba45516bb64c7717ce92
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-07-22 13:31:59 +0300

    Add javascript linter

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,19 @@
-function hamburger () {
-	const hamburgerMenu = document.getElementsByClassName('nav-icon');
-	const hamburgerMenuExit = document.getElementsByClassName('cross');
-	const hamburgerMenuItems = document.getElementsByClassName('hamburger__list-items');
-	hamburgerMenu[0].addEventListener('click', () => {
-		document.getElementsByClassName('menu')[0].style.display = 'flex';
-	});
-	hamburgerMenuExit[0].addEventListener('click', () => {
-		document.getElementsByClassName('menu')[0].style.display = 'none';
-	});
-	
-	[...hamburgerMenuItems].forEach(element => {
-		element.addEventListener('click', () => {
-			document.getElementsByClassName('menu')[0].style.display = 'none';
-		});		
-	});
+function hamburger() {
+  const hamburgerMenu = document.getElementsByClassName('nav-icon');
+  const hamburgerMenuExit = document.getElementsByClassName('cross');
+  const hamburgerMenuItems = document.getElementsByClassName('hamburger__list-items');
+  hamburgerMenu[0].addEventListener('click', () => {
+    document.getElementsByClassName('menu')[0].style.display = 'flex';
+  });
+  hamburgerMenuExit[0].addEventListener('click', () => {
+    document.getElementsByClassName('menu')[0].style.display = 'none';
+  });
+
+  [...hamburgerMenuItems].forEach((element) => {
+    element.addEventListener('click', () => {
+      document.getElementsByClassName('menu')[0].style.display = 'none';
+    });
+  });
 }
 
 hamburger();
