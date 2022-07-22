98867ef187bb3cef03b5c43897b3ed0769b6ddd6
UmerZia <omerzia73@gmail.com>
2022-07-22 15:18:15 +0500

    fix-hamburger-menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,19 @@
-// const navToggle = document.querySelector('#navToggle');
-// const nav = document.querySelector('#nav-hidden');
-// // const navIcon = document.querySelectorAll('.nav-icon');
-// // const hamburger = document.querySelector('#hamburger');
+function hamburger () {
+	const hamburgerMenu = document.getElementsByClassName('nav-icon');
+	const hamburgerMenuExit = document.getElementsByClassName('cross');
+	const hamburgerMenuItems = document.getElementsByClassName('hamburger__list-items');
+	hamburgerMenu[0].addEventListener('click', () => {
+		document.getElementsByClassName('menu')[0].style.display = 'flex';
+	});
+	hamburgerMenuExit[0].addEventListener('click', () => {
+		document.getElementsByClassName('menu')[0].style.display = 'none';
+	});
+	
+	[...hamburgerMenuItems].forEach(element => {
+		element.addEventListener('click', () => {
+			document.getElementsByClassName('menu')[0].style.display = 'none';
+		});		
+	});
+}
 
-// navToggle.addEventListener( "click", () => { //when clicked do ..
-//      alert("cliked")
-// 	nav.classList.toggle('show') //open this  
-// 	// // navIcon.forEach(icon => {
-	// 	icon.classList.toggle('hidden');
-	// })
-// 	}
-// )
- 
-// window.addEventListener("resize", () => {
-// 	if(document.body.clientWidth > 768) {
-// 		nav.classList.remove('open'); 
-// 		navIcon.forEach(icon => {
-// 			icon.classList.add('hidden')
-// 		});
-// 		hamburger.classList.remove('hidden')
-// 	}
-// })
-
-
-const navToggle = document.querySelector('#navToggle');
-
-// const nav = document.querySelector('nav');
-// const navIcon = document.querySelectorAll('.nav-icon');
-const hamburger = document.querySelector('#hamburger');
-const logo = document.querySelector('.logo');
-const menu = document.querySelector('.menu');
-const cross = document.querySelector('.cross');
-
-hamburger.addEventListener('click', () => {
-	logo.classList.add('hide');
-	hamburger.classList.add('hide');
-	menu.style.display = 'block';
-})
-
-cross.addEventListener('click', () => {
-	logo.classList.remove('hide');
-	hamburger.classList.remove('hide');
-	menu.style.display = 'none';
-})
+hamburger();
