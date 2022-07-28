69ff25418498ab11cdb4fc147aad11251a15fd8a
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-07-28 15:31:03 +0300

    Add validation with javascript

--- js/app.js
+++ js/app.js
@@ -18,3 +18,34 @@ function hamburger() {
   });
 }
 hamburger();
+
+const btnSubmit = document.getElementById('send');
+const inputEmail = document.getElementById('email');
+const EMAIL_CASE_CHECK = 'Email adress should be in lower case';
+const EMAIL_CHECK = 'Enter valid email';
+const error = document.getElementById('warning');
+
+btnSubmit.addEventListener('click', (e) => {
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const address = inputEmail.value;
+  if (address === address.toLowerCase()) {
+    if (validRegex.test(address)) {
+      error.classList.remove('.alert');
+      error.classList.add('.off-alert');
+    } else {
+      error.innerHTML = `<span id="warning">${EMAIL_CHECK}</span>`;
+      e.preventDefault();
+      error.classList.add('.alert');
+      error.classList.remove('.off-alert');
+    }
+  } else {
+    e.preventDefault();
+    error.innerHTML = `<span id="warning">${EMAIL_CASE_CHECK}</span>`;
+    error.classList.add('.alert');
+    error.classList.remove('.off-alert');
+  }
+  setTimeout(() => {
+    error.classList.remove('.alert');
+    error.classList.add('.off-alert');
+  }, 100);
+});
